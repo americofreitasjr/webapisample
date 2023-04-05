6f0fed39e12e9a803d2105ca1c2169290bb69d6e
Thay-Campos <campostay30@gmail.com>
2023-04-05 10:42:23 -0300

    Criação API Logon

---
+++ webapisample/API/LogonRequest.cs
@@ -0,0 +1,8 @@
+namespace API
+{
+    public class LogonRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ webapisample/API/LogonResponse.cs
@@ -0,0 +1,9 @@
+namespace API
+{
+    public class LogonResponse
+    {
+        public string Username { get; set; }
+        public DateTime Data { get; set; }
+        public bool Logado { get; set; }
+    }
+}
