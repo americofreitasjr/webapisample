05c7510efc0be223d1f765a26d471267c793394d
Thay-Campos <campostay30@gmail.com>
2023-04-13 11:56:29 -0300

    Lead Controller

--- webapisample/API/Controllers/LeadController.cs
+++ webapisample/API/Controllers/LeadController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API.Controllers 
 {
     [ApiController]
     [Route("[controller]")]
@@ -9,38 +9,29 @@ namespace API.Controllers
         private static readonly LeadRequest[] Leads = new LeadRequest[]
         {
             new LeadRequest{ Nome = "jr",Sobrenome="Silva", Email="campostay@gmail.com", DataNascimento="02/05/1991"},
-           
+
         };
 
         private readonly ILogger<LeadController> _logger;
 
-        public LeadController(ILogger<LeadController> logger)
+        public LeadController(ILogger<LeadController> logger) 
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "LeadController")]
-        public IEnumerable<Lead> Get() {
+        public IEnumerable<Lead> Get() 
+        {
             return Enumerable.Range(1, 5).Select(index => new Lead {
                 Lead = Leads[Random.Shared.Next(Leads.Length)]
             })
             .ToArray();
-
-
-            [HttpPost] 
-        public ActionResult<LeadResponse> Post(LeadRequest req)
-        {
-            var logado = Leads.Any(w => w.Nome == req.Nome && w.Sobrenome == req.Sobrenome && w.Email == req.Email && w.DataNascimento == req.Datanascimento);
-
-            var response = new LeadResponse() {
-                Nome = req.,
-                Logado = logado,
-                Data = DateTime.Now,
-            };
-
-            return Ok(response);
         }
-    }
+
+
 
     }
-}
+     
+
+        }
+   
