3db1828001907bf219baadabfb567bb416c8e142
Thay-Campos <campostay30@gmail.com>
2023-05-05 11:12:22 -0300

    Lead Get Post

--- webapisample/API/Controllers/LeadController.cs
+++ webapisample/API/Controllers/LeadController.cs
@@ -1,28 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
-namespace API.Controllers 
-{
+namespace API.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class LeadController : ControllerBase
-    {
+    public class LeadController : ControllerBase {
         private static readonly Lead[] CapDeLeads = new Lead[]
         {
-            new Lead{ Nome = "jr",Sobrenome="Silva", Email="campostay@gmail.com", DataNascimento=new DateTime(1991, 05, 02),},  
-           
+            new Lead{ Nome = "jr",Sobrenome="Silva", Email="campostay@gmail.com", DataNascimento=new DateTime(1991, 05, 02),},
+
         };
 
         private readonly ILogger<LeadController> _logger;
 
-        public LeadController(ILogger<LeadController> logger) 
-        {
+        public LeadController(ILogger<LeadController> logger) {
             _logger = logger;
         }
 
         [HttpGet(Name = "LeadController")]
-        public IEnumerable<Lead> Get() 
-        {
+        public IEnumerable<Lead> Get() {
             return (IEnumerable<Lead>)CapDeLeads.ToList();
 
         }
@@ -31,26 +27,28 @@ namespace API.Controllers
         public ActionResult<Lead> Post(Lead req) {
             //Se for M é Sr, se for F Sra
 
-            var idade = Math.Abs(Math.Round(((new DateTime(1985, 06, 16) - DateTime.Now).TotalDays / 365.25d), 0));
+            double idade = Calcularidade(req);
 
             Console.WriteLine(idade);
 
             if (req.Genero == "M") {
-                string mensagem = $"Olá Sr {req.Nome} {req.Sobrenome} ({idade}) Obrigado por se cadastrar. Você receberá uma confirmação pelo e-mail {req.Email}";
+                string mensagem = $"Olá Sr {req.Nome} {req.Sobrenome} ({idade} anos) Obrigado por se cadastrar. Você receberá uma confirmação pelo e-mail {req.Email}";
                 return Ok(mensagem);
             } else if (req.Genero == "F") {
-                string mensagem = $"Olá Sra {req.Nome} {req.Sobrenome} Obrigado por se cadastrar. Você receberá uma confirmação pelo e-mail {req.Email}";
+                string mensagem = $"Olá Sra {req.Nome} {req.Sobrenome} ({idade} anos) Obrigado por se cadastrar. Você receberá uma confirmação pelo e-mail {req.Email}";
                 return Ok(mensagem);
 
-             }
+            }
 
-             return Ok(); 
+            return Ok();
 
+            static double Calcularidade(Lead req) { 
+                return Math.Abs(Math.Round((req.DataNascimento - DateTime.Now).TotalDays / 365.25d, 0));
+            }
         }
 
 
     }
-     
 
- }
-   
+
+}
