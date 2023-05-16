47c374b35e41daaa0be876658c225d43b2130a0e
Thay-Campos <campostay30@gmail.com>
2023-05-16 11:32:31 -0300

    Refatoração PostLead

--- webapisample/API/Controllers/LeadController.cs
+++ webapisample/API/Controllers/LeadController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Metrics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace API.Controllers {
     [ApiController]
@@ -24,84 +25,76 @@ namespace API.Controllers {
 
         }
 
+        
+        [HttpPost]
+        public ActionResult<Lead> Post(Lead req) {
+            double idade = CalcularIdade(req);
+
+            // Se idade menor que 18 devolver mensagem
+            if (idade < 18) {
+                return Ok($"Você possui ({idade} anos) e essa idade é abaixo da permitida para se cadastrar.");
+            }
+
+            string diaSemana = TraduzirDiaSemana(req);
+
+            string mensagem = DefinirMensagemResposta(req, diaSemana, idade);
+
+            return Ok (mensagem); 
+        }
+
         private double CalcularIdade(Lead req) {
             return Math.Abs(Math.Round((req.DataNascimento - DateTime.Now).TotalDays / 365.25d, 0));
 
         }
 
-        private string TraduzirDiaSemana(Lead req, string diaSemana) {
+        private string TraduzirDiaSemana(Lead req) {
+            string diaSemana = "";
+
             switch (req.DataNascimento.DayOfWeek.ToString()) {
                 case "Sunday":
                     diaSemana = "Domingo";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
-
                 case "Monday":
                     diaSemana = "Segunda-Feira";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
 
                 case "Tuesday":
                     diaSemana = "Terça-Feira";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
 
                 case "Wednesday":
                     diaSemana = "Quarta-Feira";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
 
                 case "Thursday":
                     diaSemana = "Quinta-Feira";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
 
                 case "Friday":
                     diaSemana = "Sexta-Feira";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
 
                 case "Saturday":
                     diaSemana = "Sábado";
-                    Console.WriteLine($"Measured value is {diaSemana}");
                     break;
-
-
             }
 
             return diaSemana;
         }
 
 
-        [HttpPost]
-        public ActionResult<Lead> Post(Lead req) {
+        private string DefinirMensagemResposta(Lead req, string diaSemana, double idade) {
             //Se for M é Sr, se for F Sra
-
-            double idade = CalcularIdade(req);
-            string diaSemana = "";
-            diaSemana = TraduzirDiaSemana(req, diaSemana);
-
-            Console.WriteLine(idade);
-            // Se idade menor que 18 devolver mensagem
-            if (idade < 18) {
-                string mensagem = $"Você possui ({idade} anos) e essa idade é abaixo da permitida para se cadastrar.";
-                return Ok(mensagem);
-            }
-
             if (req.Genero == "M") {
                 string mensagem = $"Olá Sr {req.Nome} {req.Sobrenome} ({idade} anos) {diaSemana} Obrigado por se cadastrar. Você receberá uma confirmação pelo e-mail {req.Email}";
-                return Ok(mensagem);
+                return mensagem;
 
             } else if (req.Genero == "F") {
                 string mensagem = $"Olá Sra {req.Nome} {req.Sobrenome} ({idade} anos) {diaSemana} Obrigado por se cadastrar. Você receberá uma confirmação pelo e-mail {req.Email}";
-                return Ok(mensagem);
+                return mensagem;
             }
-            return Ok();
-
-            
+            return ""; 
         }
-
-
     }
 
 
